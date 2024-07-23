2662a75d9204e44faf643408b54de9ddf4a3b495
iamajjayya <ajjayya2002@gmail.com>
2024-07-23 15:57:41 +0530

    login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../Contexts/AuthProvider";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+const title  = "Login"
+const  socilTitle = "Login with social media"
+const  btnText = "Login Now"
+
+const socialList = [
+  { iconName: "icofont-facebook", siteLink: "#", className: "facebook" },
+  { iconName: "icofont-twitter", siteLink: "#", className: "twitter" },
+  { iconName: "icofont-linkedin", siteLink: "#", className: "linkedin" },
+  { iconName: "icofont-instagram", siteLink: "#", className: "instagram" },
+  { iconName: "icofont-pinterest", siteLink: "#", className: "pinterest" },
+];
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+const [errorMessage, setErrorMessage] = useState(" ")
+const {signInWithGmail , login} =  useContext(AuthContext);
+
+const  location = useLocation();
+
+const navigate  = useNavigate();
+
+const from  =  location.state?.from?.pathname || "/"
+
+const handlelogin = (e) => {
+
+
+
 }
 
-export default Login
+  return (
+    <>
+      <div className="login-section padding-tb section-bg">
+                <div className="container">
+                   <div className="account-wrapper">
+                           <h3 className="title">{title}</h3>
+                           <form className="account-form" onSubmit={handlelogin}>
+                               <div className="form-group">
+                                   <input type="email"  name="email" id="email" placeholder="Email Address * "  required />
+                               </div>
+                               <div className="form-group">
+                                   <input type="password"  name="password" id="password" placeholder="Password * "  required />
+                               </div>
+                               <div className="form-group">
+                                   <div className="d-flex justify-content-between flex-wrap pt-sm-2"> 
+                                    <div className="checkgroup">
+                                      <input type="checkbox" name="remember"  id="remember" />
+                                      <label htmlFor="remember">Remember me</label>
+                                     </div>
+
+                                     <Link to="/forgetpassword">Forget Password</Link>
+                                   </div>
+                               </div>
+                               <div className="form-group">
+                                   <button type="submit" className="d-block lab-btn "><span>{btnText}</span></button>
+                               </div>
+                           </form>
+
+                           {/* account bottom */}
+
+                           <div className="account-bottom">
+                                 <span className="d-block cate pt-10">
+                                    Dont Have an Account <Link to="/sign-up">Sign Up</Link>
+                                 </span>
+                                 <span className="or">
+                                         <span>or</span>
+                                 </span>
+                                 {/* social icons */}
+
+                                 <h5 className="subtitle">{socilTitle}</h5>
+                                 <ul className="lab-ul social-icons justify-content-center">
+                                    <li>
+                                    <a href="/" className="github" ><i className="icofont-github"></i></a>
+                                    </li>
+                                    <li>
+                                    <a href="/" className="facebook"><i className="icofont-facebook"></i></a>
+                                    </li>
+                                    <li>
+                                    <a href="/" className="linkedin"><i className="icofont-linkedin"></i></a>
+                                    </li>
+                                    <li>
+                                    <a href="/" className="instagram"><i className="icofont-instagram"></i></a>
+                                    </li>
+                                    <li>
+                                    <a href="/" className="pinterest"><i className="icofont-pinterest"></i></a>
+                                    </li>
+                                 </ul>
+                           </div> 
+
+                   </div>
+                </div>
+      </div>
+    </>
+  )
+};
+
+export default Login;
